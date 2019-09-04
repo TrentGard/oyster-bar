562094c0a3e64b35364204334361c852d6ac504b
Trent Gard <trentgard@gmail.com>
2019-09-03 19:06:44 -0500

    updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,12 +25,14 @@ const Home = () => {
           <img src={image4} alt="Home" className="carousel-image" />
         </Carousel.Item>
       </Carousel>
+      <br />
       <Row className="justify-content-md-center info">
         <Col md="auto">
           <h3>Location:</h3>
           <br />
           <p>1830 S Calhoun St</p>
           <p>Fort Wayne, IN 46802</p>
+          <br />
         </Col>
         <Col md="auto">
           <h3>Hours:</h3>
@@ -38,12 +40,14 @@ const Home = () => {
           <p>M-Th: 11 AM to 10 PM</p>
           <p>Fri: 11 AM to 11 PM</p>
           <p>Sat: 5 PM to 11 PM</p>
+          <br />
         </Col>
         <Col md="auto">
-          <h3>Reservations:</h3>
+          <h3>Reservations</h3>
+          <h3>& Caterings:</h3>
           <br />
-          <p>Call us at</p>
-          <p>(260) 744-9490</p>
+          <p>Call (260) 744-9490</p>
+          <br />
         </Col>
       </Row>
     </Container>
