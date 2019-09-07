f16acadc320669384439e452f37a29d2a6c80377
Trent Gard <trentgard@gmail.com>
2019-09-07 15:32:12 -0500

    ready to go live

--- src/pages/Social.js
+++ src/pages/Social.js
@@ -12,25 +12,25 @@ const Social = () => {
             <a href="https://www.facebook.com/The-Oyster-Bar-292501212282/" target="_blank" style={{ "color": "tan" }}><h3 className="page-title"><img src={facebookIcon} alt="Facebook" className="social-icon" /> Facebook</h3></a>
             <br />
             <Carousel controls={true} interval={4500} fade={true}>
-                <Carousel.Item>
+                <Carousel.Item className="social-container">
                     <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D10158801074134358%26set%3Da.10150421279549358%26type%3D3&width=500"
                         width="500"
                         height="370"
                         style={{ "border": "none;overflow:hidden", "background": "white" }}
                         scrolling="no"
-                        frameborder="0"
-                        allowTransparency="true"
+                        frameBorder="0"
+                        allowtransparency="true"
                         allow="encrypted-media">
                     </iframe>
                 </Carousel.Item>
-                <Carousel.Item>
-                    <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Friggo75%2Fposts%2F10219559016992779&width=500" width="500" height="679" style={{ "border": "none;overflow:hidden", "background": "white" }} scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+                <Carousel.Item className="social-container">
+                    <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Friggo75%2Fposts%2F10219559016992779&width=500" width="500" height="679" style={{ "border": "none;overflow:hidden", "background": "white" }} scrolling="no" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                 </Carousel.Item>
-                <Carousel.Item>
-                    <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Flaura.evanssteele%2Fposts%2F2282369278690079&width=500" width="500" height="661" style={{ "border": "none;overflow:hidden", "background": "white" }} scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+                <Carousel.Item className="social-container">
+                    <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Flaura.evanssteele%2Fposts%2F2282369278690079&width=500" width="500" height="661" style={{ "border": "none;overflow:hidden", "background": "white" }} scrolling="no" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                 </Carousel.Item>
-                <Carousel.Item>
-                    <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fdon.croy%2Fposts%2F10212506969461189%3A0&width=500" width="500" height="563" style={{ "border": "none;overflow:hidden", "background": "white" }} scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+                <Carousel.Item className="social-container">
+                    <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fdon.croy%2Fposts%2F10212506969461189%3A0&width=500" width="500" height="563" style={{ "border": "none;overflow:hidden", "background": "white" }} scrolling="no" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                 </Carousel.Item>
             </Carousel>
             <br />
