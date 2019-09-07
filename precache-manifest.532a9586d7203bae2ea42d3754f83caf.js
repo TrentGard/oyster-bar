9447e4c10e5fd1a419bba9d54fd70cb310024dbf
Trent Gard <trentgard@gmail.com>
2019-09-07 15:40:00 -0500

    Updates

---
+++ precache-manifest.532a9586d7203bae2ea42d3754f83caf.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179aaf745d9dad61aa517cffce17e53c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab1db0a418895fc158b7",
+    "url": "/static/css/main.45e167c7.chunk.css"
+  },
+  {
+    "revision": "dcd8b9246dc8e2bc9fec",
+    "url": "/static/js/2.d6930848.chunk.js"
+  },
+  {
+    "revision": "ab1db0a418895fc158b7",
+    "url": "/static/js/main.5b8b22ad.chunk.js"
+  },
+  {
+    "revision": "88c2959e74cff3af7755",
+    "url": "/static/js/runtime~main.a7a5477f.js"
+  },
+  {
+    "revision": "052c328117a674a300eefef0e06845d7",
+    "url": "/static/media/Carousel1.052c3281.jpg"
+  },
+  {
+    "revision": "bd166a92af0b3f739bcbaae6d61b8464",
+    "url": "/static/media/Carousel2.bd166a92.gif"
+  },
+  {
+    "revision": "ab111f15ccd5fd3ca4afc1c52d438774",
+    "url": "/static/media/Carousel3.ab111f15.gif"
+  },
+  {
+    "revision": "465361bfef587f8a5b8cb06df006c439",
+    "url": "/static/media/Carousel4.465361bf.gif"
+  },
+  {
+    "revision": "a767fe923884be0a94351f4fecf8d637",
+    "url": "/static/media/Catering_Menu1.a767fe92.jpg"
+  },
+  {
+    "revision": "7d091291d8d46ac2ff4006c7abc87b08",
+    "url": "/static/media/Catering_Menu10.7d091291.jpg"
+  },
+  {
+    "revision": "d96d5ab515f3847ad53572cf1e9a185b",
+    "url": "/static/media/Catering_Menu11.d96d5ab5.jpg"
+  },
+  {
+    "revision": "14ab3f8ca6ffb334f465779602978cb5",
+    "url": "/static/media/Catering_Menu12.14ab3f8c.jpg"
+  },
+  {
+    "revision": "d546304ba5ae3ba388bdb136fabdfc2a",
+    "url": "/static/media/Catering_Menu2.d546304b.jpg"
+  },
+  {
+    "revision": "d7ed7c7a80d2e80f496d9ecee452b6dc",
+    "url": "/static/media/Catering_Menu3.d7ed7c7a.jpg"
+  },
+  {
+    "revision": "b1ad42b23b73cb0176aba756b6533309",
+    "url": "/static/media/Catering_Menu4.b1ad42b2.jpg"
+  },
+  {
+    "revision": "d1a8a8d0992bec0aff0aa807000a91c2",
+    "url": "/static/media/Catering_Menu5.d1a8a8d0.jpg"
+  },
+  {
+    "revision": "dcf780207e5841e463b99abde697d34a",
+    "url": "/static/media/Catering_Menu6.dcf78020.jpg"
+  },
+  {
+    "revision": "d12b46c09c8dd4c2199a8f2ccc71e835",
+    "url": "/static/media/Catering_Menu7.d12b46c0.jpg"
+  },
+  {
+    "revision": "60dd28f8a10eabeb4451cb26bd3638ce",
+    "url": "/static/media/Catering_Menu8.60dd28f8.jpg"
+  },
+  {
+    "revision": "bd975fa7314452e390e52853bc33fccd",
+    "url": "/static/media/Catering_Menu9.bd975fa7.jpg"
+  },
+  {
+    "revision": "eeae06db15e5602c1008b6ea8ced79d9",
+    "url": "/static/media/Cocktails.eeae06db.jpg"
+  },
+  {
+    "revision": "8084da3bf725aacae00abfc18051b3d8",
+    "url": "/static/media/Daily_Features.8084da3b.jpg"
+  },
+  {
+    "revision": "8740bd1c8cd3ea47ecab093e05742d42",
+    "url": "/static/media/Dinner_Apps.8740bd1c.jpg"
+  },
+  {
+    "revision": "ebed61b1127484825d52c0121d0aa34f",
+    "url": "/static/media/Dinner_Meats.ebed61b1.jpg"
+  },
+  {
+    "revision": "e93127f6683d1a1bccce013633fb8000",
+    "url": "/static/media/Dinner_Seafood.e93127f6.jpg"
+  },
+  {
+    "revision": "95a8df2028fcbd5b6765b4d713f4a282",
+    "url": "/static/media/Lunch_App.95a8df20.jpg"
+  },
+  {
+    "revision": "7d6f8607e8214586d237d1dfdafb2ef4",
+    "url": "/static/media/Lunch_Entree.7d6f8607.jpg"
+  },
+  {
+    "revision": "49ba8517ef3055c29b64dae831eee5ee",
+    "url": "/static/media/Lunch_Sandwich.49ba8517.jpg"
+  },
+  {
+    "revision": "360551211c8d83beec9d6eb7cb431a8f",
+    "url": "/static/media/OB_Logo.36055121.png"
+  },
+  {
+    "revision": "d3a0b9d44b33bbc1980d9538c7529c2f",
+    "url": "/static/media/Wine_List.d3a0b9d4.jpg"
+  },
+  {
+    "revision": "7d91755166fd557af35e3dc8a36dad08",
+    "url": "/static/media/dark-paths.7d917551.png"
+  },
+  {
+    "revision": "d1ddbb0bbbea802499370deba9ef04b5",
+    "url": "/static/media/facebook_icon.d1ddbb0b.png"
+  },
+  {
+    "revision": "56ee4e05fe0b524bed7d4d81836748b3",
+    "url": "/static/media/instagram_icon.56ee4e05.png"
+  }
+]);
--- src/components/BackgroundImage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function BackgroundImage() {
-  return <img src="../public/dark-paths.png" />;
-}
-
-export default BackgroundImage;
