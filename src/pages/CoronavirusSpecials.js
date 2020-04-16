a4d9765b24ddfb60d52184db585e43607944adbd
TrentGard <trentgard@gmail.com>
2020-04-16 10:43:37 -0500

    added Thursday special

--- src/pages/CoronavirusSpecials.js
+++ src/pages/CoronavirusSpecials.js
@@ -2,7 +2,7 @@ import React from "react";
 import dailySpecial410 from "../img/410.JPG";
 import dailySpecial414 from "../img/414-1.JPG";
 import dailySpecial4142 from "../img/414-3.JPG";
-import dailySpecial5 from "../img/48-2.JPG";
+import dailySpecial416 from "../img/416.JPG";
 import dailySpecial49 from "../img/49.JPG";
 import dailySpecial7 from "../img/42.JPG";
 import dailySpecial415 from "../img/415.JPG";
@@ -14,12 +14,17 @@ function DailySpecials() {
     <Container>
       <h1 className="page-title">Daily Specials</h1>
       <br />
-      <h4 className="page-title blue">Available Wednesday (4/15)</h4>
+      <h4 className="page-title blue">Available Thursday (4/16)</h4>
       <OBCarousel className="carousel-image-menu" />
-      <img src={dailySpecial415} alt="Daily Specials" className="menu-image" />
+      <img src={dailySpecial416} alt="Daily Specials" className="menu-image" />
       <br />
       <br />
       <h1 className="page-title">Previous Specials</h1>
+      <br />
+      <h4 className="page-title blue">Wednesday (4/15)</h4>
+      <br />
+      <img src={dailySpecial415} alt="Daily Specials" className="menu-image" />
+      <br />
       <br />
       <h4 className="page-title blue">Tuesday (4/14)</h4>
       <br />
@@ -35,11 +40,6 @@ function DailySpecials() {
       <h4 className="page-title blue">Thursday (4/9)</h4>
       <br />
       <img src={dailySpecial49} alt="Daily Specials" className="menu-image" />
-      <br />
-      <br />
-      <h4 className="page-title blue">Wednesday (4/8, LUNCH AND DINNER)</h4>
-      <br />
-      <img src={dailySpecial5} alt="Daily Specials" className="menu-image" />
     </Container>
   );
 }
