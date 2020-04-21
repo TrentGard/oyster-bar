495aa3ce88d831afe6b22596dbc8857a99b2ec9b
TrentGard <trentgard@gmail.com>
2020-04-21 15:04:06 -0500

    updated Tuesday special

--- src/pages/CoronavirusDinner.js
+++ src/pages/CoronavirusDinner.js
@@ -9,8 +9,8 @@ const CoronavirusDinner = () => {
     <Container>
       <h1 className="page-title">Dinner Take-Out</h1>
       <br />
-      <h4 className="page-title blue">Available Monday through Saturday from 4 PM to 7 PM</h4>
-      <h4 className="page-title blue">Note: Due to supply issues we will be offering oysters as FRIED ONLY until further notice.</h4>
+      <h4 className="page-title blue">Available Tuesday through Friday from 4 PM to 7 PM</h4>
+      <h4 className="page-title blue">Note: OYSTERS ARE BACK (except for Parmesan and Beinville)!</h4>
       <OBCarousel className="carousel-image-menu" />
       <img src={COVIDApps} alt="Dinner Apps" className="menu-image" />
       <img src={COVIDDinner} alt="Dinner" className="menu-image" />
