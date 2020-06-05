fa605b6177ce2211b27624544b890f3ace710e01
TrentGard <trentgard@gmail.com>
2020-06-05 10:46:55 -0500

    added 6/5 specials

--- src/pages/CoronavirusSpecials.js
+++ src/pages/CoronavirusSpecials.js
@@ -1,15 +1,12 @@
 import React from "react";
+import dailySpecial65 from "../img/65.JPG";
+import dailySpecial652 from "../img/652.JPG";
 import dailySpecial64 from "../img/64.JPG";
 import dailySpecial63 from "../img/63.jpg";
 import dailySpecial62 from "../img/62.JPG";
 import dailySpecial5292 from "../img/529-2.JPG";
 import dailySpecial5293 from "../img/529-3.JPG";
 import dailySpecial529 from "../img/529.JPG";
-import dailySpecial5284 from "../img/528-4.JPG";
-import dailySpecial5283 from "../img/528-3.JPG";
-import dailySpecial5282 from "../img/528-2.JPG";
-import dailySpecial528 from "../img/528.JPG";
-import dailySpecial527 from "../img/527-2.JPG";
 import lobsterRollPlate from "../img/LobsterRollPlate.JPG";
 import OBCarousel from "../components/OBCarousel";
 import { Container } from "react-bootstrap";
@@ -19,17 +16,22 @@ function DailySpecials() {
     <Container>
       <h1 className="page-title">Daily Specials</h1>
       <br />
-      <h4 className="page-title blue">Available Thursday (6/4)</h4>
+      <h4 className="page-title blue">Available Friday (6/5)</h4>
       <OBCarousel className="carousel-image-menu" />
-      <img src={dailySpecial64} alt="Daily Specials" className="menu-image" />
+      <img src={dailySpecial65} alt="Daily Specials" className="menu-image" />
+      <img src={dailySpecial652} alt="Daily Specials" className="menu-image" />
       <br />
       <br />
       <h1 className="page-title">Previous Specials</h1>
       <br />
+      <h4 className="page-title blue">Thursday (6/4)</h4>
+      <br />
+      <img src={dailySpecial64} alt="Daily Specials" className="menu-image" />
+      <br />
+      <br />
       <h4 className="page-title blue">Wednesday (6/3)</h4>
       <br />
       <img src={dailySpecial63} alt="Daily Specials" className="menu-image" />
-      <img src={lobsterRollPlate} alt="Daily Specials" className="menu-image" />
       <br />
       <br />
       <h4 className="page-title blue">Tuesday (6/2)</h4>
@@ -42,14 +44,6 @@ function DailySpecials() {
       <img src={dailySpecial5292} alt="Daily Specials" className="menu-image" />
       <img src={dailySpecial5293} alt="Daily Specials" className="menu-image" />
       <img src={dailySpecial529} alt="Daily Specials" className="menu-image" />
-      <br />
-      <br />
-      <h4 className="page-title blue">Thursday (5/28)</h4>
-      <br />
-      <img src={dailySpecial5283} alt="Daily Specials" className="menu-image" />
-      <img src={dailySpecial5284} alt="Daily Specials" className="menu-image" />
-      <img src={dailySpecial5282} alt="Daily Specials" className="menu-image" />
-      <img src={dailySpecial528} alt="Daily Specials" className="menu-image" />
     </Container>
   );
 }
